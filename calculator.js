7e519f3bdff09b51f241b2e3f4fc0dd71d32b5a7
Aaron Janusitis <aaronjanusitis@gmail.com>
2023-10-02 14:08:40 -0400

    Completed calculator project

--- calculator.js
+++ calculator.js
@@ -15,6 +15,7 @@ function divide(num1, num2) {
 }
 
 function operate(num1, num2, operator) {
+    if (operator == null) return;
     if(operator == '+') {
         return add(num1, num2);
     }
@@ -30,23 +31,69 @@ function operate(num1, num2, operator) {
     else {
         return 'Invalid operator';
     }
+    calcOperator = null;
 }
 
 function changeDisplay() {
-    displayText.textContent = displayValue;
+    tempDisplay = Number(((Number(displayValue)).toFixed(6)).toString());
+    displayText.textContent = tempDisplay;
+}
+
+function resetDisplay() {
+    displayValue = '';
+    displayText.textContent = '';
+    resetToggle = false;
 }
 
 function clearInput() {
-    calcOperator = '';
+    calcOperator = null;
     calcNum1 = '';
     calcNum2 = '';
     displayValue = '';
+    displayText.textContent = '0';
+    resetToggle = false;
+}
+
+function backSpace() {
+    displayValue = displayValue.slice(0, -1);
+    if (displayValue.endsWith ('.')) displayValue = displayValue.slice(0, -1);
+    changeDisplay();
+}
+
+function numButtonClick(input) {
+    if (resetToggle == true) resetDisplay();
+    if (displayValue.includes('.') && input == '.') return;
+    displayValue += input;
+    if (displayValue == '.') return;
+    changeDisplay();  
+}
+
+function operatorClick(operatorInput) {
+    if (calcOperator != null && resetToggle == false) {
+        if (calcOperator == '/' && Number(displayValue) == 0) {
+            alert("You can't divide by 0! Resetting calculator")
+            clearInput();
+            return;
+        }
+        else {
+            calcNum1 = Number(displayValue);
+            displayValue = operate(calcNum2, calcNum1, calcOperator);
+            displayValue = displayValue.toString();
+            console.log(typeof displayValue);
+            changeDisplay();
+            calcOperator = null;
+        }        
+    }
+    calcNum2 = Number(displayValue);
+    calcOperator = operatorInput;
+    resetToggle = true;
 }
 
 let calcNum1 = '';
 let calcNum2 = '';
-let calcOperator = '';
+let calcOperator = null;
 let displayValue = '';
+let resetToggle = false;
 
 const btn7 = document.querySelector('#btn_7');
 const btn8 = document.querySelector('#btn_8');
@@ -64,94 +111,80 @@ const btnTimes = document.querySelector('#btn_times');
 const btnDivide = document.querySelector('#btn_divide');
 const btnClear = document.querySelector('#btn_clear');
 const btnEquals = document.querySelector('#btn_equals');
+const btnDecimal = document.querySelector('#btn_decimal');
+const btnBack = document.querySelector('#btn_backspace');
+
 const displayText = document.querySelector('.display_text');
 
 btn7.addEventListener('click', () => {
-    displayValue += '7';
-    changeDisplay();  
+    numButtonClick('7');
 });
 
 btn8.addEventListener('click', () => {
-    displayValue += '8';
-    changeDisplay();  
+    numButtonClick('8');
 });
 
 btn9.addEventListener('click', () => {
-    displayValue += '9';
-    changeDisplay();  
+    numButtonClick('9'); 
 });
 
 btn4.addEventListener('click', () => {
-    displayValue += '4';
-    changeDisplay();  
+    numButtonClick('4');
 });
 
 btn5.addEventListener('click', () => {
-    displayValue += '5';
-    changeDisplay();  
+    numButtonClick('5'); 
 });
 
 btn6.addEventListener('click', () => {
-    displayValue += '6';
-    changeDisplay();  
+    numButtonClick('6'); 
 });
 
 btn1.addEventListener('click', () => {
-    displayValue += '1';
-    changeDisplay();  
+    numButtonClick('1'); 
 });
 
 btn2.addEventListener('click', () => {
-    displayValue += '2';
-    changeDisplay();  
+    numButtonClick('2');
 });
 
 btn3.addEventListener('click', () => {
-    displayValue += '3';
-    changeDisplay();  
+    numButtonClick('3'); 
 });
 
 btn0.addEventListener('click', () => {
-    displayValue += '0';
-    changeDisplay();  
+    numButtonClick('0'); 
+});
+
+btnDecimal.addEventListener('click', () => {
+    numButtonClick('.');
 });
 
 btnPlus.addEventListener('click', () => {
-    calcOperator = '+';
-    calcNum1 = Number(displayValue);
-    displayValue = '';
-    changeDisplay(); 
-
+    operatorClick('+');
 });
 
 btnMinus.addEventListener('click', () => {
-    calcOperator = '-';
-    calcNum1 = Number(displayValue);
-    displayValue = '';
-    changeDisplay();
+    operatorClick('-');
 });
 
 btnTimes.addEventListener('click', () => {
-    calcOperator = '*';
-    calcNum1 = Number(displayValue);
-    displayValue = '';
-    changeDisplay();
+    operatorClick('*');
 });
 
 btnDivide.addEventListener('click', () => {
-    calcOperator = '/';
-    calcNum1 = Number(displayValue);
-    displayValue = '';
-    changeDisplay();
+    operatorClick('/');
 });
 
 btnClear.addEventListener('click', () => {
     clearInput();
-    changeDisplay();  
+});
+
+btnBack.addEventListener('click', () => {
+    backSpace();
 });
 
 btnEquals.addEventListener('click', () => {
-    calcNum2 = Number(displayValue);
-    displayValue = operate(calcNum1, calcNum2, calcOperator);
-    changeDisplay();
+    operatorClick();
 });
+
